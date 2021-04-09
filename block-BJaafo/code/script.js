0a27ef3a7551043767233d3d093b8e40f2914bb8
Bitopan Deka <bitopanxon1@gmail.com>
2021-04-09 08:47:57 +0530

    Completed Assignment Writing Test

---
+++ block-BJaafo/code/script.js
@@ -0,0 +1,21 @@
+function getFullName(firstName, lastName) {
+  return firstName + " " + lastName;
+}
+
+let result = getFullName("Bitopan", "Deka");
+let expected = "BitopanDeka";
+
+if (result !== expected) {
+  throw new Error(`${result} is not equal to ${expected}`);
+}
+
+function getTotalAmount(amount, lastName) {
+  return firstName + " " + lastName;
+}
+
+let result = getFullName("Bitopan", "Deka");
+let expected = "BitopanDeka";
+
+if (result !== expected) {
+  throw new Error(`${result} is not equal to ${expected}`);
+}
