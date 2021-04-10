80ed6907a5d1a7d04cfb1d37d3377459bf72b7ea
Bitopan Deka <bitopanxon1@gmail.com>
2021-04-10 09:14:56 +0530

    Refactor

--- block-BJaafo/code/script.js
+++ block-BJaafo/code/script.js
@@ -9,12 +9,12 @@ if (result !== expected) {
   throw new Error(`${result} is not equal to ${expected}`);
 }
 
-function getTotalAmount(amount, lastName) {
-  return firstName + " " + lastName;
+function getTotalAmount(amount, taxRate) {
+  return amount + (amount * taxRate) ;
 }
 
-let result = getFullName("Bitopan", "Deka");
-let expected = "BitopanDeka";
+let result = getTotalAmount(100, 5/100);
+let expected = 102;
 
 if (result !== expected) {
   throw new Error(`${result} is not equal to ${expected}`);
